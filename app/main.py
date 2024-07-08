d48cbc36f9ba80b90f60a6acf7ea2849d8d53d92
deduu <dedy.ariansyah1@gmail.com>
2024-07-08 11:09:18 +0700

    fix docker

--- app/main.py
+++ app/main.py
@@ -36,4 +36,4 @@ async def root():
     return {"greeting": "Hello, World!", "message": "This is a service to extract the first frame of a video!"}
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host=host, port=port, log_level="info")
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, log_level="info")
